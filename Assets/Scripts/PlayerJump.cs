06aa0bb8955d944df3f453bcbd4e077626a3a807
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2021-09-20 10:13:29 +0200

    Player Jump Once

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerJump : MonoBehaviour
+{
+    private bool _isGrounded;
+    [SerializeField] private Vector2 jumpForce;
+    private Rigidbody2D _rb2d;
+
+    private void Start()
+    { 
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            _rb2d.AddForce(jumpForce, ForceMode2D.Impulse);
+            _isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground") && _isGrounded == false) _isGrounded = true;
+    }
+}
--- Assets/Scripts/Stats.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Stats", menuName = "ScriptableObjects/Stats")]
-public class Stats : ScriptableObject
-{
-    public int attempts;
-}
